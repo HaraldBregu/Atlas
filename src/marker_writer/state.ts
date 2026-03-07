86e1ebad69213e00e7757bbcbe8bca62bbf4dab6
harald bregu <harald.bregu@gmail.com>
2026-03-07 16:08:57 +0100

    Clean up unused state fields and remove obsolete input-parser and stitcher nodes

--- src/marker_writer/state.ts
+++ src/marker_writer/state.ts
@@ -3,9 +3,7 @@ import type {
   CursorInfo,
   Intent,
   DocumentState,
-  Context,
   StyleProfile,
-  Structure,
   AssembledPrompt,
   DiffInfo,
 } from '@/marker_writer/types';
@@ -15,28 +13,19 @@ export const WriterState = Annotation.Root({
   rawInput: Annotation<string>,
   userInstruction: Annotation<string>,
 
-  // Layer 1
+  // Parsed
   cursorInfo: Annotation<CursorInfo>,
   intent: Annotation<Intent>,
   documentState: Annotation<DocumentState>,
-
-  // Layer 2
-  context: Annotation<Context>,
   styleProfile: Annotation<StyleProfile>,
-  structure: Annotation<Structure>,
   targetLength: Annotation<number>,
 
-  // Layer 3
+  // Generated
   assembledPrompt: Annotation<AssembledPrompt>,
   generatedText: Annotation<string>,
-
-  // Layer 4
   processedText: Annotation<string>,
-  coherencePass: Annotation<boolean>,
-  coherenceFeedback: Annotation<string>,
-  retryCount: Annotation<number>,
 
-  // Layer 5
+  // Output
   finalDocument: Annotation<string>,
   diff: Annotation<DiffInfo>,
   changeDescription: Annotation<string>,
