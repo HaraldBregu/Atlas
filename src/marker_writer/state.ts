2c6b12150ff5ae1c8d11a321abae7ddffe4e3d12
harald bregu <harald.bregu@gmail.com>
2026-03-07 20:56:57 +0100

    Simplify marker_writer to minimal state and remove analyzer-generator architecture

--- src/marker_writer/state.ts
+++ src/marker_writer/state.ts
@@ -1,20 +1,9 @@
 import { Annotation } from '@langchain/langgraph';
-import type {
-  Intent,
-  StyleProfile,
-  AssembledPrompt,
-} from '@/marker_writer/types';
 
 export const WriterState = Annotation.Root({
-  rawInput: Annotation<string>,
-  userInstruction: Annotation<string>,
-
-  intent: Annotation<Intent>,
-  styleProfile: Annotation<StyleProfile>,
-  targetLength: Annotation<number>,
-  assembledPrompt: Annotation<AssembledPrompt>,
+  inputText: Annotation<string>,
+  instruction: Annotation<string>,
   generatedText: Annotation<string>,
-  finalDocument: Annotation<string>,
 });
 
 export type WriterStateValue = typeof WriterState.State;
