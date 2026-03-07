b2294735896e21e0a76be03d82fac8a19c9ffe9d
harald bregu <harald.bregu@gmail.com>
2026-03-07 16:18:41 +0100

    Simplify marker writer state by removing unused fields and imports

--- src/marker_writer/state.ts
+++ src/marker_writer/state.ts
@@ -1,34 +1,16 @@
 import { Annotation } from '@langchain/langgraph';
-import type {
-  CursorInfo,
-  Intent,
-  DocumentState,
-  StyleProfile,
-  AssembledPrompt,
-  DiffInfo,
-} from '@/marker_writer/types';
+import type { StyleProfile, AssembledPrompt } from '@/marker_writer/types';
 
 export const WriterState = Annotation.Root({
-  // Input
   rawInput: Annotation<string>,
+  afterText: Annotation<string>,
   userInstruction: Annotation<string>,
 
-  // Parsed
-  cursorInfo: Annotation<CursorInfo>,
-  intent: Annotation<Intent>,
-  documentState: Annotation<DocumentState>,
   styleProfile: Annotation<StyleProfile>,
   targetLength: Annotation<number>,
-
-  // Generated
   assembledPrompt: Annotation<AssembledPrompt>,
   generatedText: Annotation<string>,
-  processedText: Annotation<string>,
-
-  // Output
   finalDocument: Annotation<string>,
-  diff: Annotation<DiffInfo>,
-  changeDescription: Annotation<string>,
 });
 
 export type WriterStateValue = typeof WriterState.State;
