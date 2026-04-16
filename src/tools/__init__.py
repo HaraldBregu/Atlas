657673c261dfd7af8fe0e15af21b128f7085f15f
harald bregu <harald.bregu@gmail.com>
2026-04-16 14:18:47 +0200

    chore: add __init__.py to make tools a Python package

---
+++ src/tools/__init__.py
@@ -0,0 +1,4 @@
+from src.tools.types import Tool
+from src.tools.registry import register_tool, get_tool, get_all_tools, tool_to_function
+
+__all__ = ["Tool", "register_tool", "get_tool", "get_all_tools", "tool_to_function"]
